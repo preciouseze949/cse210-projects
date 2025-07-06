b28f99f71d66a2fb698e69152c01750c83274e2c
preciouseze949 <preciousanietem@gmail.com>
2025-07-06 04:46:08 +0100

    modified:   week01/Exercise1/Program.cs 	modified:   week01/Exercise2/Program.cs 	modified:   week01/Exercise3/Program.cs 	modified:   week01/Exercise4/Program.cs 	modified:   week01/Exercise5/Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your First name? ");
+        string Fname = Console.ReadLine();
+        Console.WriteLine("What is your Last name? ");
+        string Lname = Console.ReadLine();
+        Console.WriteLine($"Your name is {Lname}, {Fname} {Lname}");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,86 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 97)
+        {
+            letter = "A+";
+        }
+        else if (percent < 93 && percent >= 90)
+        {
+            letter = "A-";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 87)
+        {
+            letter = "B+";
+        }
+        else if (percent < 83 && percent >= 80)
+        {
+            letter = "B-";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 77)
+        {
+            letter = "C+";
+        }
+        else if (percent < 73 && percent >= 70)
+        {
+            letter = "c-";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else if (percent >= 67)
+        {
+            letter = "D+";
+        }
+        else if (percent < 63 && percent >= 60)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else if (percent >= 97)
+        {
+            Console.WriteLine("You passed with a plus mark!");
+        }
+        else if (percent >= 87)
+        {
+            Console.WriteLine("You passed with a plus mark!");
+        }
+        else if (percent >= 77)
+        {
+            Console.WriteLine("You passed with a plus mark!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
